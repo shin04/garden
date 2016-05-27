7159afae0b28fb3017e6c5da60864c665d8a0ea1
shin04 <daikon0413@gmail.com>
2016-05-27 14:36:57 +0900

    Initial Commit

---
+++ garden/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  garden
+//
+//  Created by 梶原大進 on 2016/05/27.
+//  Copyright © 2016年 梶原大進. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
